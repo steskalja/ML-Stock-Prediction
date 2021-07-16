54eac63754d7acc2a8449f56d7a6ae3bd65899bd
John <steskalj@outlook.com>
2021-07-15 20:02:59 -0400

    Updates to the app

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import getgraph as gg
 
 app = Flask(__name__)
  
-@app.route("/")
-def hello():
-    return "Welcome to TutsPlus!"
-    
-@app.route('/showLineChart')
-def line():
+@app.route("/", methods =["GET", "POST"])
+def cg():
     graphJSON = gg.get_graph()
+    if request.method == "POST":
+        ntp = request.form.get("num")
+        predictJSON = gg.create_predict(int(ntp))
+        return render_template('index.html',graphJSON=graphJSON,predictJSON=predictJSON )
     return render_template('index.html',graphJSON=graphJSON)
--- flask/stockmodel.py
+++ flask/stockmodel.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import LSTM,Dense, Dropout, BatchNormalization
 from tensorflow.python.keras.preprocessing.sequence import TimeseriesGenerator
-from tensorflow.python.keras.utils.data_utils import Sequence
 
 def get_script_path():
     return os.path.dirname(getsourcefile(lambda:0))
@@ -26,3 +25,21 @@ def get_tf(data):
     dn = np.reshape(data, (len(data),1))
     tsg = TimeseriesGenerator(dn, dn, length=look_back, batch_size=1)
     return tsg
+
+def prediction(num_prediction, mod, td):
+    test_data = td
+    prediction_list = test_data[-4:]
+    
+    for _ in range(num_prediction):
+        x = prediction_list[-4:]
+        x = x.reshape((1, 4, 1))
+        out = mod.predict(x)[0][0]
+        prediction_list = np.append(prediction_list, out)
+    prediction_list = prediction_list[4-1:]
+        
+    return prediction_list
+    
+def prediction_dates(num_prediction,tdt):
+    last_date = tdt.values[-1]
+    prediction_dates = pd.date_range(last_date, periods=num_prediction+1).tolist()
+    return prediction_dates
